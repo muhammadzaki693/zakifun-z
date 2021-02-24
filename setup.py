ba412492f9e41235427fb57d4527ba62441bc567
muhammadzaki693 <rzaki9353@gmail.com>
2021-02-24 17:47:26 +0700

    intial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="",
+    author_email="rzaki9353@gmail.com",
+    description="A small package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/muhammadzaki693/zakifun-z",
+    project_urls={
+        "Bug Tracker": "https://github.com/muhammadzaki693/zakifun-z/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.x',
+)
